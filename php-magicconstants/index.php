2318f4b6a11f23d6727ae0daef7f5bc11a08c455
yemreu <yunusemreuzun@outlook.com>
2021-09-19 16:30:13 +0300

    Magic Constants; __LINE__

---
+++ php-magicconstants/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8;" />
+    <meta http-equiv="Content-Language" content="tr" />
+    <meta charset="utf-8" />
+    <title>Php</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <script type="text/javascript" lang="javascript" src="script.js"></script>
+  </head>
+  <body>
+    <?php
+      print(__LINE__ . "</br>");
+    ?>
+  </body>
+</html>
