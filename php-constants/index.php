da8e84d55029f22610664adc3eb8fb94ad318922
yemreu <yunusemreuzun@outlook.com>
2021-09-19 15:46:33 +0300

    Constants; define()

---
+++ php-constants/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8;" />
+    <meta http-equiv="Content-Language" content="tr" />
+    <meta charset="utf-8" />
+    <title>Php</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <script type="text/javascript" lang="javascript" src="script.js"></script>
+  </head>
+  <body>
+    <?php
+      define("js","Javascript");
+      print(js);
+    ?>
+  </body>
+</html>
